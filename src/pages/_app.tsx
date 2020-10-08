ac562d5a371d87b89ea87f0c9f83f1d0fd8ec38b
DuGlaser <duglaser0618@gmail.com>
2020-10-08 23:08:39 +0900

    :recycle: Fix the MyApp component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import { store } from '@/store';
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
-export const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
   );
 };
+
+export default MyApp;
